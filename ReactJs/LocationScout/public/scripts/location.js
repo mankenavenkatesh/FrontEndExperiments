b8abdc1c86aadc6533fd4f4bb5a02b6195be1d90
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2016-08-12 09:09:03 +0530

    hello world react js location scout app

---
+++ ReactJs/LocationScout/public/scripts/location.js
@@ -0,0 +1,12 @@
+var Location=React.createClass({
+    render : function(){
+      return(
+        <div>
+          <h1>Hello World</h1>
+        </div>
+      );
+    }
+}
+);
+
+ReactDOM.render(<Location />, document.getElementById('content'));
--- ReactJs/LocationScout/server.js
+++ ReactJs/LocationScout/server.js
@@ -1,14 +1,3 @@
-/**
- * This file provided by Facebook is for non-commercial testing and evaluation
- * purposes only. Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
 var fs = require('fs');
 var path = require('path');
@@ -16,7 +5,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var COMMENTS_FILE = path.join(__dirname, 'comments.json');
+var LOCATIONS_FILE = path.join(__dirname, 'locations.json');
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -35,8 +24,8 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
+app.get('/api/locations', function(req, res) {
+  fs.readFile(LOCATIONS_FILE, function(err, data) {
     if (err) {
       console.error(err);
       process.exit(1);
@@ -45,28 +34,28 @@ app.get('/api/comments', function(req, res) {
   });
 });
 
-app.post('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
+app.post('/api/locations', function(req, res) {
+  fs.readFile(LOCATIONS_FILE, function(err, data) {
     if (err) {
       console.error(err);
       process.exit(1);
     }
-    var comments = JSON.parse(data);
+    var locations = JSON.parse(data);
     // NOTE: In a real implementation, we would likely rely on a database or
     // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
     // treat Date.now() as unique-enough for our purposes.
-    var newComment = {
+    var newLocation = {
       id: Date.now(),
       author: req.body.author,
       text: req.body.text,
     };
-    comments.push(newComment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+    locations.push(newLocation);
+    fs.writeFile(LOCATIONS_FILE, JSON.stringify(locations, null, 4), function(err) {
       if (err) {
         console.error(err);
         process.exit(1);
       }
-      res.json(comments);
+      res.json(locations);
     });
   });
 });
